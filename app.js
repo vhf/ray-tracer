045c63318c0b2fb0a3f7cf6f7c769e407d4fb8ca
Victor Felder <victorfelder@gmail.com>
2014-11-10 22:11:04 -0500

    tom is Intersecting

--- app.js
+++ app.js
@@ -67,8 +67,45 @@ function Sphere(center, radius) {
     if (val1 > 0 || val2 > 0) {
       return true;
     }
-
   };
+
+  this.tomIsIntersecting = function(vector) {
+
+    var v1 = vector.direction;
+    var v2 = vector.origin;
+
+    var x1 = v1.x;
+    var y1 = v1.y;
+    var z1 = v1.z;
+    var x2 = v2.x;
+    var y2 = v2.y;
+    var z2 = v2.z;
+
+    var x3 = this.center.x;
+    var y3 = this.center.y;
+    var z3 = this.center.z;
+    var r = this.radius;
+
+    var a = Math.pow(x2 - x1, 2) + Math.pow((y2 - y1), 2) + Math.pow((z2 - z1), 2);
+    var b = 2*((x2 - x1)*(x1 - x3) + (y2 - y1)*(y1 - y3) + (z2 - z1)*(z1 - z3) );
+    var c = Math.pow(x3, 2) + Math.pow(y3, 2) + Math.pow(z3, 2) + Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(z1, 2) - 2*(x3*x1 + y3*y1 + z3*z1) - Math.pow(r, 2);
+    var radicand = Math.pow(b, 2) - 4*a*c;
+
+    function xyz_from_u(u) {
+      return [x1 + u * (x2 - x1), y1 + u * (y2 - y1), z1 + u * (z2 - z1)];
+    }
+
+    if (radicand < 0){
+      return false;
+    } else if (radicand === 0){
+      return true;
+    } else if (radicand > 0) {
+      return true;
+    } else {
+      throw "logic error!";
+    }
+  };
+
 }
 
 var createCamera = function(width) {
@@ -78,7 +115,7 @@ var createCamera = function(width) {
     camera[i] = [];
     for (var j = 0; j < width; j++) {
       var origin = new Point(0, 0, 0);
-      var direction = new Point(i/width, j/width, 1).times(500);
+      var direction = new Point(i-250, j-250, 499);
       camera[i][j] = new Vector(origin, direction);
     }
   }
@@ -86,14 +123,22 @@ var createCamera = function(width) {
 };
 
 var camera = createCamera(500);
-var sphere = new Sphere(new Point(10, 10, 10), 10);
+var objects = [];
+//objects.push(new Sphere(new Point(0, 0, 1000), 100));
+objects.push(new Sphere(new Point(100, 50, 1000), 10));
+objects.push(new Sphere(new Point(100, 150, 1000), 10));
+objects.push(new Sphere(new Point(250, 50, 1000), 10));
+objects.push(new Sphere(new Point(250, 150, 1000), 10));
+//objects.push(new Sphere(new Point(100, 100, 500), 10));
 var canvas = document.getElementById("canvas").getContext('2d');
 
 for (var x = 0; x < camera.length; x++) {
   for (var y = 0; y < camera.length; y++) {
-    if (sphere.isIntersecting(camera[x][y])) {
-      canvas.fillRect(camera[x][y].direction.x, camera[x][y].direction.y,
-      (camera[x][y].direction.x)+1, (camera[x][y].direction.y)+1);
+    for (var i = 0; i < objects.length; i++) {
+      if (objects[i].tomIsIntersecting(camera[x][y])) {
+        canvas.fillRect(camera[x][y].direction.x, camera[x][y].direction.y,
+        (camera[x][y].direction.x)+1, (camera[x][y].direction.y)+1);
+      }
     }
   }
 }
